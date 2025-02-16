380ac8610602b136a72cdfd02b2cd713ea0cfb22
Tanvir Ahamed <ahamedtanvir374@gmail.com>
2025-02-16 11:17:40 +0600

    Added DATABASE_URL to .env file

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { Application, NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
-import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import routes from './app/routes';
-import cookieParser from 'cookie-parser';
 import cron from 'node-cron';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import { AppointmentServices } from './app/modules/appointment/appointment.services';
+import routes from './app/routes';
 import { errorlogger } from './shared/logger';
-import config from './config';
 
 const app: Application = express();
 
@@ -28,7 +27,7 @@ app.use('/api/v1', routes);
 
 app.get('/test', async (req: Request, res: Response) => {
   res.status(200).json({
-    message: 'Server working....!',
+    message: 'Server working.... perfectly!',
   });
 });
 
