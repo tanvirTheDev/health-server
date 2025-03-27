3259e8c21c664eb7f282dfd83400e99efffe6261
Tanvir Ahamed <ahamedtanvir374@gmail.com>
2025-03-28 02:51:31 +0600

    add meiliseach live server

--- src/shared/meilisearch.ts
+++ src/shared/meilisearch.ts
@@ -1,8 +1,8 @@
 import { MeiliSearch } from 'meilisearch';
 
 const meiliClient = new MeiliSearch({
-  host: 'http://localhost:7700',
-  apiKey: 'masterKey',
+  host: process.env.MEILISEARCH_HOST as string,
+  apiKey: process.env.MEILISEARCH_API_KEY,
 });
 
 export default meiliClient;
